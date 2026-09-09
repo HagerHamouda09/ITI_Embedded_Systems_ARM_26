8ddd2a56e4ac316e342a7d5f8fdbce255b49c213
Hager Adel <115931896+HagerHamouda09@users.noreply.github.com>
2026-09-09 15:06:33 +0300

    Update FMI_prg.c

--- Drivers/Cortex-M4/MCAL/FMI/FMI_prg.c
+++ Drivers/Cortex-M4/MCAL/FMI/FMI_prg.c
@@ -14,60 +14,75 @@
 
 void MFMI_vMassErase(void)
 {
+	//1- wait on busy flag =1
 	while(GET_BIT(FMI->SR, BSY));
-
+	
+	//2- if locked CR register -> unlock
 	if(GET_BIT(FMI->CR,LOCK))
 	{
 		FMI->KEYR = KEY1;
 		FMI->KEYR = KEY2;
 	}
 
+	//3- Set the MER bit in the FLASH_CR
 	SET_BIT(FMI->CR, MER);
 
+	//4- Set the STRT bit in the FLASH_CR
 	SET_BIT(FMI->CR,STRT);
-
+	
+	//5- Wait for the BSY bit to be cleared
 	while(GET_BIT(FMI->SR, BSY));
-
+	
+	//6- Turn off mass erase
 	CLR_BIT(FMI->CR, MER);
 }
 
 void MFMI_vSectorErase(Sector_t A_xSectorNo)
 {
+	//1- wait on busy flag =1
 	while(GET_BIT(FMI->SR, BSY));
-
+	
+	//2- if locked CR register -> unlock
 	if(GET_BIT(FMI->CR,LOCK))
 	{
 		FMI->KEYR = KEY1;
 		FMI->KEYR = KEY2;
 	}
 
+	//3- Select sector to erase
 	FMI->CR &=~(0b1111<<SNB);
 	FMI->CR |= (A_xSectorNo<<SNB);
 
+	//4- Select sector erase
 	SET_BIT(FMI->CR, SER);
-
+	
+	//5- Set the STRT bit in the FLASH_CR
 	SET_BIT(FMI->CR,STRT);
-
+	
+	//6- Wait for the BSY bit to be cleared
 	while(GET_BIT(FMI->SR, BSY));
 
+	//7- Turn off mass erase
 	CLR_BIT(FMI->CR, SER);
 }
 
 void MFMI_vProgramFlash(u32 A_u32Address, u16* A_PtrData, u16 DataLength)
 {
-
+	//1- wait on busy flag =1
 	while(GET_BIT(FMI->SR, BSY));
-
+	
+	//2- if locked CR register -> unlock
 	if(GET_BIT(FMI->CR,LOCK))
 	{
 		FMI->KEYR = KEY1;
 		FMI->KEYR = KEY2;
 	}
-
+	
+	//3- Select width (16 bit) to be programmed
 	FMI->CR &=~(0b11<<PSIZE);
 	FMI->CR |= (1<<PSIZE);
 
-
+	// Select programming mode
 	SET_BIT(FMI->CR, PG);
 
 	// arr[2]={1,2}
@@ -83,6 +98,7 @@ void MFMI_vProgramFlash(u32 A_u32Address, u16* A_PtrData, u16 DataLength)
 		while(GET_BIT(FMI->SR, BSY));
 
 	}
+	// Stop programming mode
 	CLR_BIT(FMI->CR, PG);
 
 }
